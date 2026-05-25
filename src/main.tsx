6a164450e98f1c0b74e271f59671f3e60a60977b
luizmartinellipontes-art <luiz.martinelli.pontes@escola.pr.gov.br>
2026-05-25 11:07:29 -0300

    main

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import {strictmode} from 'react'
+import {creatRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+creatRoot(document.getElementById(Root)!).render
+<strictmode>
+    <App>
+        </scrictmode>
